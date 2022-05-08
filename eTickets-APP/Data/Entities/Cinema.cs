32a6f0050833620f435b6e40eff3624e84507594
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-08 15:10:21 +0300

    Add Cinema entity

---
+++ eTickets-APP/Data/Entities/Cinema.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
