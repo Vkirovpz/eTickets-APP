55250d411015c0746289709ab7318275718d2419
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-08 21:20:14 +0300

    Add First Migration

--- eTickets-APP/eTickets-APP/Startup.cs
+++ eTickets-APP/eTickets-APP/Startup.cs
@@ -3,6 +3,7 @@ namespace eTickets_APP
     using eTickets.Data;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -19,7 +20,7 @@ namespace eTickets_APP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
             services.AddControllersWithViews();
         }
