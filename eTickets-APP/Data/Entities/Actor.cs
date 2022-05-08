19fc1704c927a457d54765d30ba77f26547dda3c
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-08 15:05:57 +0300

    Add Actor entity

---
+++ eTickets-APP/Data/Entities/Actor.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public int Bio { get; set; }
+    }
+}
