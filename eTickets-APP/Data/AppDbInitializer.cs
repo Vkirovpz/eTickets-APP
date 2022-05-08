8a140e16904cf10f464ddc5f007a36288142b97e
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-08 21:50:32 +0300

    Add Data Initializer

---
+++ eTickets-APP/Data/AppDbInitializer.cs
@@ -0,0 +1,44 @@
+﻿namespace eTickets.Data
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
+
+    public class AppDbInitializer
+    {
+        public static void Seed (IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Cinemas.Any())
+                {
+
+                }
+
+                if (!context.Actors.Any())
+                {
+
+                }
+
+                if (!context.Producers.Any())
+                {
+
+                }
+
+                if (!context.Movies.Any())
+                {
+
+                }
+
+                if (!context.Actors_Movies.Any())
+                {
+
+                }
+            }
+        }
+    }
+}
