54c5505d30e184d139c647a59a087a622309d3f0
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-14 09:47:53 +0300

    Add ProducersController

---
+++ eTickets-APP/eTickets-APP/Controllers/ProducersController.cs
@@ -0,0 +1,11 @@
+﻿namespace eTickets_APP.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class ProducersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
