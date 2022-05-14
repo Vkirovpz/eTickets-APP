8a0293a0bd04e7a76c8afe08a2d2ba42bb8f57c1
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-05-14 09:51:38 +0300

    Add Cinemas And Movies Controllers

---
+++ eTickets-APP/eTickets-APP/Controllers/CinemasController.cs
@@ -0,0 +1,11 @@
+﻿namespace eTickets_APP.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class CinemasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eTickets-APP/eTickets-APP/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿namespace eTickets_APP.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
