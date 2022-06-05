28cdef5ff73b23c0f86ad2594925d6f45697530c
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-06-05 07:59:32 +0300

    Add Shopping Cart

---
+++ eTickets-APP/eTickets-Domain/Cart/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿namespace eTickets_Domain.Cart
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using eTickets.Data;
+    using eTickets.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+            return total;
+        }
+    }
+}
