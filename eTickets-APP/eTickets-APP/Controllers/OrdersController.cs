7eb68a45890b87638d395456baefc6dd4baac963
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-06-09 19:01:48 +0300

    Add OrderIndex Action and All orders view

--- eTickets-APP/eTickets-APP/Controllers/OrdersController.cs
+++ eTickets-APP/eTickets-APP/Controllers/OrdersController.cs
@@ -21,6 +21,13 @@
             _shoppingCart = shoppingCart;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         { 
             var items = _shoppingCart.GetShoppingCartItems();
@@ -67,5 +74,7 @@
 
             return View("OrderCopmpleted");
         }
+
+
     }
 }
